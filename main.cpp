0e8ee2f17a0757d6e60b3b6660983e8bb536c8e7
ceorron <rcookman@hotmail.com>
2022-03-10 22:22:18 +0000

    added tests for hybrid rotate merge sort and similar

--- main.cpp
+++ main.cpp
@@ -271,6 +271,54 @@ int main() {
         std::cout << "sorted : " << stlib::is_sorted(vec.begin(), vec.end()) << std::endl;
     }
     {
+        std::cout << "test stackless rotate merge sort" << std::endl;
+        //test rotate merge sort
+        std::vector<uint32_t> vec;
+        for(uint32_t i = 0; i < count; ++i)
+            vec.push_back(rand());
+
+        {
+            timer tmr;
+            stlib::stackless_rotate_merge_sort(vec.begin(), vec.end());
+        }
+
+		if(verbose) {
+			std::cout << "[" << std::endl;
+			for(uint32_t i = 0; i < count; ++i) {
+				std::cout << "[ " << vec[i] << "], ";
+				if(i > 0 && i % 5 == 0)
+					std::cout << std::endl;
+			}
+			std::cout << "]" << std::endl;
+		}
+
+        std::cout << "sorted : " << stlib::is_sorted(vec.begin(), vec.end()) << std::endl;
+    }
+    {
+        std::cout << "test hybrid stackless rotate merge sort" << std::endl;
+        //test rotate merge sort
+        std::vector<uint32_t> vec;
+        for(uint32_t i = 0; i < count; ++i)
+            vec.push_back(rand());
+
+        {
+            timer tmr;
+            stlib::hybrid_stackless_rotate_merge_sort(vec.begin(), vec.end());
+        }
+
+		if(verbose) {
+			std::cout << "[" << std::endl;
+			for(uint32_t i = 0; i < count; ++i) {
+				std::cout << "[ " << vec[i] << "], ";
+				if(i > 0 && i % 5 == 0)
+					std::cout << std::endl;
+			}
+			std::cout << "]" << std::endl;
+		}
+
+        std::cout << "sorted : " << stlib::is_sorted(vec.begin(), vec.end()) << std::endl;
+    }
+    {
         std::cout << "test zip sort" << std::endl;
         //test zip sort
         std::vector<uint32_t> vec;
